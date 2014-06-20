748d2e29b1aba2713966171ed1dbef14d6010de0
Saisneha Koppaka <saisnehakoppaka@Saisnehas-MacBook-Air.local>
2014-06-20 14:00:48 -0400

    Changes made

--- generate_data.sh
+++ generate_data.sh
@@ -1,6 +1,6 @@
 # Exercise: Use a for loop to create a text file in that should be called data1.txt, and contain lines 1 to 10. append with a >> . take our simple_forloop and cat that into generate_data.sh
-
 #This is just a new comment to add a new file to git
+#This is fun! Yay!
 
 #touch data1.txt
 cat data1.txt
